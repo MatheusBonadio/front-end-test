a58cfb86dca1f430545cc74b099f0257d2871f51
Matheus Bonadio <matheus_lima_bonadio_xd@hotmail.com>
2023-01-16 12:22:44 -0300

    Update getters and setters

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 An object-oriented stove with **Javascript, HTML and CSS** by Mentes notáveis.
 
-<img src="https://i.imgur.com/8JKM0vU.png">
+<img src="https://i.imgur.com/4wG80sz.png">
--- src/app/Stove.js
+++ src/app/Stove.js
@@ -15,12 +15,76 @@ class Stove {
         this.startsStove();
     }
 
+    set color(string) {
+        this._color = string;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set width(string) {
+        this._width = string;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set height(string) {
+        this._height = string;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set length(string) {
+        this._length = string;
+    }
+
+    get length() {
+        return this._length;
+    }
+
     set brand(string) {
         this._brand = string;
     }
 
     get brand() {
         return this._brand;
+    }
+
+    set burners(int) {
+        this._burners = int;
+    }
+
+    get burners() {
+        return this._burners;
+    }
+
+    set lighters(array) {
+        this._lighters = array;
+    }
+
+    get lighters() {
+        return this._lighters;
+    }
+
+    set oven(array) {
+        this._oven = array;
+    }
+
+    get oven() {
+        return this._oven;
+    }
+
+    set lamp(bool) {
+        this._lamp = bool;
+    }
+
+    get lamp() {
+        return this._lamp;
     }
 
     // Return data from Stove in console.log
@@ -44,6 +108,7 @@ class Stove {
         document.querySelector('.stoveBody').setAttribute('style', 'background-color: ' + this._color);
         document.querySelector('#stoveWidth').innerHTML = this._width;
         document.querySelector('#stoveHeight').innerHTML = this._height;
+        document.querySelector('#stoveLength').innerHTML = this._length;
         document.querySelector('#brand').innerHTML = this._brand;
     }
 
